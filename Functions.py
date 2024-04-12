6abb6a07f81a2995c58ccf17fb2d195e3b7aa87e
FunnylionJ9 <jassimkrajab@gmail.com>
2024-04-12 14:26:24 +0100

    Functions explanation

---
+++ Functions.py
@@ -0,0 +1,9 @@
+
+def function1(): #defines the code
+    variable1 = int(input("Give number "))
+    variable2 = int(input("Give number "))
+    variable3 = variable1*variable2
+    print(variable3)
+
+function1() #executes code
+function1() #executes code again
